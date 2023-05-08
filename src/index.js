64007f6c25889581bb39ebb74358db4558ca0db8
norma <diazyamileth896@gmail.com>
2023-05-08 06:38:21 -0600

    Codigo base y configuracion inicial

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const $app = document.getElementById('app');
+const API = "https://api.escuelajs.co/api/v1/products?offset=0&limit=10";
+
+const main = async () => {
+    const response = await fetch(API);
+    const products = await response.json();
+
+    const output = products?.map((product) => {
+        return `
+        <artcicle class="card">
+            <img src="${product.images[0]}" alt="${product.title}" />
+            <h2>${product.title} <small>Precio $ ${product.price}</small> 
+            </h2>
+            </artcicle>
+            `
+    }).join("");
+    let newItem = document.createElement("section");
+    newItem.classList.add("Items");
+    newItem.innerHTML = output;
+    $app.appendChild(newItem);
+}
+
+main();
